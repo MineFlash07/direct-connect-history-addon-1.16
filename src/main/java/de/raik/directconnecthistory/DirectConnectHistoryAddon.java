2482c005b247ab9bc0140b144c4bf29a1ed8fd63
MineFlash07 <mineflash07@gmail.com>
2021-03-06 16:02:02 +0100

    added bascic structure

---
+++ src/main/java/de/raik/directconnecthistory/DirectConnectHistoryAddon.java
@@ -0,0 +1,88 @@
+package de.raik.directconnecthistory;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import de.raik.directconnecthistory.config.GlobalAddonConfig;
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+import net.minecraft.client.Minecraft;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DirectConnectHistoryAddon extends LabyModAddon {
+
+    //Settings
+    private int historyLength = 5;
+    private final ArrayList<String> history = new ArrayList<>();
+
+    private GlobalAddonConfig globalConfig;
+
+    @Override
+    public void onEnable() {
+        this.globalConfig = new GlobalAddonConfig(this);
+    }
+
+    @Override
+    public void loadConfig() {
+        this.globalConfig.compare();
+        JsonObject config = this.getConfig();
+
+        this.historyLength = config.has("amount") ? config.get("amount").getAsInt() : this.historyLength;
+
+        if (!config.has("servers")) {
+            this.checkDirectConnect();
+            return;
+        }
+        this.history.clear();
+        JsonArray historyConfig = config.getAsJsonArray("servers");
+        for (JsonElement historyElement: historyConfig) {
+            this.history.add(historyElement.getAsString());
+        }
+
+        this.checkDirectConnect();
+    }
+
+    private void checkDirectConnect() {
+        String newDirectConnect = Minecraft.getInstance().gameSettings.lastServer;
+        if (!this.getConfig().has("lastServer")) {
+            this.getConfig().addProperty("lastServer", newDirectConnect);
+            this.saveConfig();
+            return;
+        }
+        String lastDirectConnect = this.getConfig().get("lastServer").getAsString();
+        this.addServer(lastDirectConnect, newDirectConnect);
+    }
+
+    public void addServer(String lastServer, String newIp) {
+        if (newIp.equals(lastServer)) {
+            return;
+        }
+        this.history.remove(newIp);
+        if (!lastServer.equals("")) {
+            this.history.remove(lastServer);
+            this.history.add(0, lastServer);
+        }
+        while (this.history.size() > this.historyLength) {
+            this.history.remove(this.history.size() - 1);
+        }
+
+        JsonArray servers = new JsonArray();
+        this.history.forEach(servers::add);
+        this.getConfig().add("servers", servers);
+        this.getConfig().addProperty("lastServer", lastServer);
+
+        this.saveConfig();
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+
+    @Override
+    public void saveConfig() {
+        this.globalConfig.save();
+    }
+}
---
+++ src/main/java/de/raik/directconnecthistory/config/GlobalAddonConfig.java
@@ -0,0 +1,58 @@
+package de.raik.directconnecthistory.config;
+
+import com.google.gson.*;
+import net.labymod.api.LabyModAddon;
+
+import java.io.*;
+
+public class GlobalAddonConfig {
+
+    private static final String CONFIG_DIRECTORY = "addons-config-global";
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final JsonParser JSON_PARSER = new JsonParser();
+
+    private final File configFile;
+    private final LabyModAddon addon;
+    private JsonObject internalConfig;
+
+    public GlobalAddonConfig(LabyModAddon addon) {
+        this.addon = addon;
+        this.configFile = new File("LabyMod/" + CONFIG_DIRECTORY, addon.about.name + ".json");
+
+        File directory = this.configFile.getParentFile();
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        this.loadConfig();
+    }
+
+    private void loadConfig() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(this.configFile)))) {
+            this.internalConfig = (JsonObject) JSON_PARSER.parse(reader);
+        } catch (IOException | JsonParseException | ClassCastException exception) {
+            this.internalConfig = new JsonObject();
+        }
+        this.save();
+    }
+
+    public void compare() {
+        this.internalConfig.entrySet().forEach(entry -> {
+            if (!this.addon.getConfig().has(entry.getKey())) {
+                this.addon.getConfig().add(entry.getKey(), entry.getValue());
+            }
+        });
+    }
+
+    public void save() {
+        this.addon.getConfig().entrySet().forEach(entry -> this.internalConfig.add(entry.getKey(), entry.getValue()));
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.configFile))) {
+            writer.write(GSON.toJson(this.addon.getConfig()));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
